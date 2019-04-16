4c0b1c40ff7ac103a8c9e0170f77d0e131493049
Claudio Costa <claudio.costa@senior.com.br>
2019-04-16 14:47:39 -0300

    Initial commit

--- basico-backend/java/impl/src/main/java/br/com/senior/furb/basico/FurbBasicoServer.java
+++ basico-backend/java/impl/src/main/java/br/com/senior/furb/basico/FurbBasicoServer.java
@@ -2,6 +2,7 @@ package br.com.senior.furb.basico;
 
 import br.com.senior.messaging.model.Server;
 import br.com.senior.messaging.model.ServiceDescription;
+
 //http://dontpad.com/pomxmlserver
 //http://dontpad.com/serverconf
 @ServiceDescription(domain = BasicoConstants.DOMAIN, name = BasicoConstants.SERVICE, packages = {
